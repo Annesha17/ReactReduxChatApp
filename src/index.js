cc85d0113e3d8aaf669600e59c794a9552792e86
annesha <annesha.k17@gmail.com>
2018-01-11 00:01:40 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
+import React from "react";
+import ReactDOM from "react-dom";
+// Readers: To prepare this project for building along in `./App.js`:
+// [1] Comment out this line:
+import App from './App';
+// [2] Un-comment this line:
+// import App from "./App";
+
+import "./index.css";
+
+import "./semantic-dist/semantic.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
